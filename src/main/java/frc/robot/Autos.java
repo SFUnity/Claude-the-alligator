b548b95e5aa38417adb8843590edb321ee66c375
MrTinker64 <avi.micah@gmail.com>
2026-01-15 18:27:54 -0800

    first take of Autos

---
+++ src/main/java/frc/robot/Autos.java
@@ -0,0 +1,91 @@
+package frc.robot;
+
+import static edu.wpi.first.wpilibj2.command.Commands.*;
+
+import choreo.auto.AutoFactory;
+import choreo.auto.AutoRoutine;
+import choreo.auto.AutoTrajectory;
+import choreo.trajectory.SwerveSample;
+import choreo.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.drive.Drive;
+import frc.robot.util.AllianceFlipUtil;
+import frc.robot.util.AlwaysLoggedTunableNumber;
+import frc.robot.util.LoggedAutoChooser;
+import frc.robot.util.PoseManager;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
+
+public class Autos {
+  private final Drive drive;
+  private final PoseManager poseManager;
+
+  private final AutoFactory factory;
+  private final LoggedAutoChooser chooser;
+
+  private final LoggedDashboardChooser<Command> nonChoreoChooser =
+      new LoggedDashboardChooser<Command>("Non-Choreo Chooser");
+  private static final boolean isChoreoAuto = true;
+
+  public static boolean moveRight = false;
+  public static boolean moveLeft = false;
+
+  public Autos(
+      Drive drive,
+      PoseManager poseManager) {
+    this.drive = drive;
+    this.poseManager = poseManager;
+
+    factory =
+        new AutoFactory(
+            poseManager::getPose,
+            poseManager::setPose,
+            drive::followTrajectory,
+            true,
+            drive,
+            (Trajectory<SwerveSample> traj, Boolean bool) -> {
+              Logger.recordOutput(
+                  "Drive/Choreo/Active Traj",
+                  (AllianceFlipUtil.shouldFlip() ? traj.flipped() : traj).getPoses());
+              Logger.recordOutput(
+                  "Drive/Choreo/Current Traj End Pose",
+                  traj.getFinalPose(AllianceFlipUtil.shouldFlip()).get());
+              Logger.recordOutput(
+                  "Drive/Choreo/Current Traj Start Pose",
+                  traj.getInitialPose(AllianceFlipUtil.shouldFlip()).get());
+            });
+
+    /* Set up main choreo routines */
+    chooser = new LoggedAutoChooser("ChoreoChooser");
+    // chooser.addRoutine("Example Auto Routine", this::exampleAutoRoutine);
+
+    if (!DriverStation.isFMSAttached()) {
+      // Set up test choreo routines
+      chooser.addRoutine("StraightLine", this::StraightLine);
+      chooser.addRoutine("Spin", this::Spin);
+
+      // SysID & non-choreo routines
+      if (!isChoreoAuto) {
+        nonChoreoChooser.addOption("Module Turn Tuning", drive.tuneModuleTurn());
+        nonChoreoChooser.addOption("Module Drive Tuning", drive.tuneModuleDrive());
+
+        // Set up SysId routines
+        nonChoreoChooser.addOption(
+            "Drive Wheel Radius Characterization", drive.wheelRadiusCharacterization());
+        nonChoreoChooser.addOption(
+            "Drive Simple FF Characterization", drive.feedforwardCharacterization());
+      }
+    }
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    return isChoreoAuto ? chooser.selectedCommandScheduler() : nonChoreoChooser.get();
+  }
+}
