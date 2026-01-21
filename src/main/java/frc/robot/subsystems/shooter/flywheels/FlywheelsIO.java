db8252082d5c909f241a23c638977439fbefec08
MINAJECHUNG <mchung27@urbanschool.org>
2026-01-20 19:31:08 -0800

    motion magic!

--- src/main/java/frc/robot/subsystems/shooter/flywheels/FlywheelsIO.java
+++ src/main/java/frc/robot/subsystems/shooter/flywheels/FlywheelsIO.java
@@ -2,15 +2,13 @@ package frc.robot.subsystems.shooter.flywheels;
 
 public interface FlywheelsIO {
     public static class FlywheelsIOOutputs{
-        public double topVelocityRotsPerSec = 0.0;
-        public double topAppliedVolts = 0.0;
-        public double topCurrentAmps = 0.0;
+        public double velocityRotsPerSec = 0.0;
+        public double appliedVolts = 0.0;
+        public double currentAmps = 0.0;
 
-        public double bottomVelocityRotsPerSec = 0.0;
-        public double bottomAppliedVolts = 0.0;
-        public double bottomCurrentAmps = 0.0;
+
     }
     public default void updateInputs(FlywheelsIOOutputs inputs){}
 
-    public default void runVolts(){}
+    public default void runVeloctiy(double velocity){}
 }
--- src/main/java/frc/robot/subsystems/shooter/flywheels/FlywheelsIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/shooter/flywheels/FlywheelsIOTalonFX.java
@@ -4,59 +4,50 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.google.errorprone.annotations.OverridingMethodsMustInvokeSuper;
 
 public class FlywheelsIOTalonFX implements FlywheelsIO{
-    private final TalonFX topMotor = new TalonFX(0);
-    private final TalonFX bottomMotor = new TalonFX(0);
-    private final VoltageOut topVoltageOut = new VoltageOut(0).withEnableFOC(true).withUpdateFreqHz(0);
-    private final VoltageOut bottomVoltageOut = new VoltageOut(0).withEnableFOC(true).withUpdateFreqHz(0);
-    public FlywheelsIOTalonFX(){
-        var topTalonFXConfigs = new TalonFXConfiguration();
+    private final TalonFX talonFX;
+    private final VoltageOut voltageOut = new VoltageOut(0).withEnableFOC(true).withUpdateFreqHz(0);
+    private final MotionMagicVelocityVoltage motionMagic = new MotionMagicVelocityVoltage(0);
+    public FlywheelsIOTalonFX(double kV, double kP, double kD, int id){
+        talonFX = new TalonFX(id);
+        var talonFXConfigs = new TalonFXConfiguration();
 
-        var slot0Configs = topTalonFXConfigs.Slot0;
-        slot0Configs.kS = 0; // Add 0.25 V output to overcome static friction
-        slot0Configs.kV = 0.12; // A velocity target of 1 rps results in 0.12 V output
-        slot0Configs.kA = 0.01; // An acceleration of 1 rps/s requires 0.01 V output
-        slot0Configs.kP = 4.8; // A position error of 2.5 rotations results in 12 V output
-        slot0Configs.kI = 0; // no output for integrated error
-        slot0Configs.kD = 0.1; // A velocity error of 1 rps results in 0.1 V output
+        var slot0Configs = talonFXConfigs.Slot0;
+        slot0Configs.kS = 0; 
+        slot0Configs.kV = kV; 
+        slot0Configs.kA = 0.0; 
+        slot0Configs.kP = kP; 
+        slot0Configs.kI = 0; 
+        slot0Configs.kD = kD; 
 
         // set Motion Magic settings
-        var motionMagicConfigs = topTalonFXConfigs.MotionMagic;
+        var motionMagicConfigs = talonFXConfigs.MotionMagic;
         motionMagicConfigs.MotionMagicCruiseVelocity = 80; // Target cruise velocity of 80 rps
         motionMagicConfigs.MotionMagicAcceleration = 160; // Target acceleration of 160 rps/s (0.5 seconds)
         motionMagicConfigs.MotionMagicJerk = 1600; // Target jerk of 1600 rps/s/s (0.1 seconds)
 
-        topMotor.getConfigurator().apply(topTalonFXConfigs);
+        talonFX.getConfigurator().apply(talonFXConfigs);
 
-        var bottomTalonFXConfigs = new TalonFXConfiguration();
-
-        var slot1Configs = bottomTalonFXConfigs.Slot1;
-        slot1Configs.kS = 0; // Add 0.25 V output to overcome static friction
-        slot1Configs.kV = 0.12; // A velocity target of 1 rps results in 0.12 V output
-        slot1Configs.kA = 0.01; // An acceleration of 1 rps/s requires 0.01 V output
-        slot1Configs.kP = 4.8; // A position error of 2.5 rotations results in 12 V output
-        slot1Configs.kI = 0; // no output for integrated error
-        slot1Configs.kD = 0.1; // A velocity error of 1 rps results in 0.1 V output
-
-        // set Motion Magic settings
-        var bottomMotionMagicConfigs = bottomTalonFXConfigs.MotionMagic;
-        motionMagicConfigs.MotionMagicCruiseVelocity = 80; // Target cruise velocity of 80 rps
-        motionMagicConfigs.MotionMagicAcceleration = 160; // Target acceleration of 160 rps/s (0.5 seconds)
-        motionMagicConfigs.MotionMagicJerk = 1600; // Target jerk of 1600 rps/s/s (0.1 seconds)
-
-        topMotor.getConfigurator().apply(bottomTalonFXConfigs);
+        
     }
     @Override
     public void updateInputs(FlywheelsIOOutputs inputs){
-        inputs.topAppliedVolts = topMotor.getMotorVoltage().getValueAsDouble();
-        inputs.topCurrentAmps = topMotor.getSupplyCurrent().getValueAsDouble();
-        inputs.topVelocityRotsPerSec = topMotor.getVelocity().getValueAsDouble();
-
-        inputs.bottomAppliedVolts = bottomMotor.getMotorVoltage().getValueAsDouble();
-        inputs.bottomCurrentAmps = bottomMotor.getSupplyCurrent().getValueAsDouble();
-        inputs.bottomVelocityRotsPerSec = bottomMotor.getVelocity().getValueAsDouble();
+        inputs.appliedVolts = talonFX.getMotorVoltage().getValueAsDouble();
+        inputs.currentAmps = talonFX.getSupplyCurrent().getValueAsDouble();
+        inputs.velocityRotsPerSec = talonFX.getVelocity().getValueAsDouble();
     }
+
+    @Override 
+    public void runVeloctiy(double velocity){
+        talonFX.setControl(motionMagic.withVelocity(velocity));
+    }
+
+    public void idle(){
+       talonFX.setControl(motionMagic.withVelocity(0)); 
+    }
+
 
 
 
