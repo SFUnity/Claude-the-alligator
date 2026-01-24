9e348c3bc501774bfe9d7e6bc88ed1cb815a64b4
Gabkwo <kwongabriel2010@gmail.com>
2026-01-24 12:35:51 -0800

    add basic names for commands

--- src/main/java/frc/robot/RobotCommands.java
+++ src/main/java/frc/robot/RobotCommands.java
@@ -1,5 +1,17 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+
 public class RobotCommands {
-    
+    public static Command intake() {
+        return Commands.none();
+    }
+
+    public static Command climb() {
+        return Commands.none();
+    }
+    public static Command score() {
+        return Commands.none();
+    }
 }
