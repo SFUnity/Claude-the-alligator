720c77059ef2c9e2a286f1ae6e5b7d02322fca60
TheDeadChicken <mishi.doron@gmail.com>
2026-01-24 14:50:27 -0800

    Setup SpindexerIO

--- src/main/java/frc/robot/subsystems/rollers/spindexer/SpindexerIO.java
+++ src/main/java/frc/robot/subsystems/rollers/spindexer/SpindexerIO.java
@@ -1,5 +1,19 @@
 package frc.robot.subsystems.rollers.spindexer;
 
-public class SpindexerIO {
+import org.littletonrobotics.junction.AutoLog;
 
+public interface SpindexerIO {
+
+    @AutoLog
+    public static class SpindexerIOInputs {
+        public double velocityRotsPerSec = 0.0;
+        public double appliedVolts = 0.0;
+        public double currentAmps = 0.0;
+    }
+
+    public default void updateInputs(SpindexerIOInputs inputs) {}
+
+    public default void spin(double velocity) {}
+
+    public default void stop() {}
 }
