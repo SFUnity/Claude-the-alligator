48276b44489b09fe5deca44a71c51ffdc0703b9f
TheDeadChicken <mishi.doron@gmail.com>
2026-01-24 14:27:21 -0800

    Reset Spindexers

--- src/main/java/frc/robot/subsystems/rollers/spindexer/Spindexer.java
+++ src/main/java/frc/robot/subsystems/rollers/spindexer/Spindexer.java
@@ -1,41 +1,6 @@
 package frc.robot.subsystems.rollers.spindexer;
 
-import java.util.function.DoubleSupplier;
-
-import frc.robot.subsystems.rollers.GenericRoller;
-import frc.robot.subsystems.rollers.GenericRoller.VoltageGoal;
-import frc.robot.util.LoggedTunableNumber;
-
-public class Spindexer extends GenericRoller<Spindexer.Goal> {
-    
-    
-    public enum Goal implements VoltageGoal {
-        IDLING(() -> 0.0),
-        FLOOR_INTAKING(new LoggedTunableNumber("Indexer/FloorIntakingVoltage", 6.0)),
-        STATION_INTAKING(new LoggedTunableNumber("Indexer/StationIntakingVoltage", -6.0)),
-        SHOOTING(new LoggedTunableNumber("Indexer/ShootingVoltage", 12.0)),
-        EJECTING(new LoggedTunableNumber("Indexer/EjectingVoltage", -8.0));
-
-        private final DoubleSupplier voltageSupplier;
-
-        private Goal(DoubleSupplier voltageSupplier) {
-            this.voltageSupplier = voltageSupplier;
-        }
-        public DoubleSupplier getVoltageSupplier() {
-            return voltageSupplier;
-        }
-        
-    }
-    public Spindexer.Goal getGoal() {
-        return goal;
-    }
-
-    
-
-  private Spindexer.Goal goal = Spindexer.Goal.IDLING;
-
-  public Spindexer(IndexerIO io) {
-    super("Indexer", io);
-  }
+public class Spindexer {
+   
 
 }
