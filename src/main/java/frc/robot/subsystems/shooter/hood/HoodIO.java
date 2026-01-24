dea6760485654999f61fe242fe35a3835581b9d2
MINAJECHUNG <mchung27@urbanschool.org>
2026-01-24 14:25:38 -0800

    create hood IO

--- src/main/java/frc/robot/subsystems/shooter/hood/HoodIO.java
+++ src/main/java/frc/robot/subsystems/shooter/hood/HoodIO.java
@@ -3,6 +3,15 @@ package frc.robot.subsystems.shooter.hood;
 import org.littletonrobotics.junction.AutoLog;
 
 public interface HoodIO {
-   @AutoLog
-    
+  @AutoLog
+  public static class HoodIOInputs {
+    public double positionDeg = 0;
+    public double appliedVolts = 0;
+    public double statorCurrent = 0;
+    public double supplyCurrent = 0;
+  }
+
+  public default void updateInputs(HoodIOInputs inputs) {}
+
+  public default void setPosition(double positionDeg) {}
 }
