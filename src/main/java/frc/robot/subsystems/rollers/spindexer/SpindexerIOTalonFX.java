916c3dbbd7e0797696708dc3cf875c2894b2b7b9
SeanC0112 <146582100+SeanC0112@users.noreply.github.com>
2026-01-25 13:02:49 -0800

    format

--- src/main/java/frc/robot/subsystems/rollers/spindexer/SpindexerIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/rollers/spindexer/SpindexerIOTalonFX.java
@@ -1,29 +1,32 @@
 package frc.robot.subsystems.rollers.spindexer;
 
+import static frc.robot.Constants.loopPeriodSecs;
+import static frc.robot.subsystems.rollers.spindexer.SpindexerConstants.*;
+
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
-import static frc.robot.subsystems.rollers.spindexer.SpindexerConstants.*;
-import static frc.robot.Constants.loopPeriodSecs;
 
 public class SpindexerIOTalonFX implements SpindexerIO {
-    private final TalonFX talon = new TalonFX(0);
+  private final TalonFX talon = new TalonFX(0);
 
-    private final VoltageOut voltageOut =
+  private final VoltageOut voltageOut =
       new VoltageOut(0).withEnableFOC(true).withUpdateFreqHz(loopPeriodSecs);
 
-    @Override
-    public void updateInputs(SpindexerIOInputs inputs) {
-        inputs.appliedVolts = talon.getMotorVoltage().getValueAsDouble();
-        inputs.statorCurrentAmps = talon.getStatorCurrent().getValueAsDouble();
-        inputs.supplyCurrentAmps = talon.getSupplyCurrent().getValueAsDouble();
-        inputs.velocityRotsPerSec = talon.getVelocity().getValueAsDouble();
-    }
-    @Override
-    public void runVolts(double voltage) {
-        talon.setControl(voltageOut.withOutput(voltage));
-    }
-    @Override
-    public void stop() {
-        talon.setControl(voltageOut.withOutput(0.0));   
-    }
+  @Override
+  public void updateInputs(SpindexerIOInputs inputs) {
+    inputs.appliedVolts = talon.getMotorVoltage().getValueAsDouble();
+    inputs.statorCurrentAmps = talon.getStatorCurrent().getValueAsDouble();
+    inputs.supplyCurrentAmps = talon.getSupplyCurrent().getValueAsDouble();
+    inputs.velocityRotsPerSec = talon.getVelocity().getValueAsDouble();
+  }
+
+  @Override
+  public void runVolts(double voltage) {
+    talon.setControl(voltageOut.withOutput(voltage));
+  }
+
+  @Override
+  public void stop() {
+    talon.setControl(voltageOut.withOutput(0.0));
+  }
 }
