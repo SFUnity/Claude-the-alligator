9189e9a1e67abd67160fa49a7a78ced6b48732ae
Lucas Lee <llee28@urbanschool.org>
2026-01-25 14:53:40 -0800

    format + add overrides for intake

--- src/main/java/frc/robot/subsystems/rollers/intake/IntakeIOSim.java
+++ src/main/java/frc/robot/subsystems/rollers/intake/IntakeIOSim.java
@@ -1,5 +1,26 @@
 package frc.robot.subsystems.rollers.intake;
 
-public class IntakeIOSim {
-    
+import frc.robot.subsystems.rollers.kicker.KickerIO.KickerIOInputs;
+
+public class IntakeIOSim implements IntakeIO {
+  private double appliedVolts = 0.0;
+  private double currentAmps = 0.0;
+
+  public IntakeIOSim() {}
+
+  @Override
+  public void updateInputs(KickerIOInputs inputs) {
+    inputs.appliedVolts = appliedVolts;
+    inputs.currentAmps = currentAmps;
+  }
+
+  @Override
+  public void stop() {
+    appliedVolts = 0;
+  }
+
+  @Override
+  public void runVolts(double volts) {
+    appliedVolts = volts;
+  }
 }
