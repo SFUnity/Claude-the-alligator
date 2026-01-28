1d8492e8bf9af8d269d51717b6f1b80dc43a861a
TheDeadChicken <mishi.doron@gmail.com>
2026-01-28 08:53:51 -0800

    Setup ClimbIOTalonFX

--- src/main/java/frc/robot/subsystems/climb/climbIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/climb/climbIOTalonFX.java
@@ -1,5 +1,26 @@
 package frc.robot.subsystems.climb;
 
-public class climbIOTalonFX {
+import static frc.robot.Constants.loopPeriodSecs;
 
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+public class climbIOTalonFX implements climbIO {
+  private final TalonFX talon = new TalonFX(0);
+
+  private final VoltageOut voltageOut =
+      new VoltageOut(0).withEnableFOC(true).withUpdateFreqHz(loopPeriodSecs);
+
+  @Override
+  public void updateInputs(climbIOInputs inputs) {
+    inputs.appliedVolts = talon.getMotorVoltage().getValueAsDouble();
+    inputs.statorCurrentAmps = talon.getStatorCurrent().getValueAsDouble();
+    inputs.supplyCurrentAmps = talon.getSupplyCurrent().getValueAsDouble();
+    inputs.velocityRotsPerSec = talon.getVelocity().getValueAsDouble();
+  }
+
+  @Override
+  public void runVolts(double voltage) {
+    talon.setControl(voltageOut.withOutput(voltage));
+  }
 }
