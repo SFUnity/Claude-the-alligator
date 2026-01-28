724d7523cb54e9c8746dd6d5ff9a1eade74bf7aa
TheDeadChicken <mishi.doron@gmail.com>
2026-01-27 17:24:10 -0800

    update runSpindexer to runVolts and add the neccesary constants

--- src/main/java/frc/robot/subsystems/rollers/spindexer/Spindexer.java
+++ src/main/java/frc/robot/subsystems/rollers/spindexer/Spindexer.java
@@ -1,8 +1,8 @@
 package frc.robot.subsystems.rollers.spindexer;
 
+import static frc.robot.subsystems.rollers.spindexer.SpindexerConstants.*;
+
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Spindexer extends SubsystemBase {
@@ -18,8 +18,8 @@ public class Spindexer extends SubsystemBase {
     io.updateInputs(inputs);
   }
 
-  public Command runSpindexer(double speed) {
-    return run(() -> io.runVolts(speed));
+  public Command runVolts() {
+    return run(() -> io.runVolts(spindexerSpeedVolts.get()));
   }
 
   public Command stopSpindexer() {
--- src/main/java/frc/robot/subsystems/rollers/spindexer/SpindexerConstants.java
+++ src/main/java/frc/robot/subsystems/rollers/spindexer/SpindexerConstants.java
@@ -1,5 +1,10 @@
 package frc.robot.subsystems.rollers.spindexer;
 
+import frc.robot.util.LoggedTunableNumber;
+
 public class SpindexerConstants {
   public static final double talonID = 0.0;
+
+  public static final LoggedTunableNumber spindexerSpeedVolts =
+      new LoggedTunableNumber("Spindexer/rollerSpeed", 2);
 }
