357e9fad27d9fb8784236f7ee370fb2b418accad
TheDeadChicken <mishi.doron@gmail.com>
2026-01-28 08:51:21 -0800

    Setup climbIO

--- src/main/java/frc/robot/subsystems/climb/climbIO.java
+++ src/main/java/frc/robot/subsystems/climb/climbIO.java
@@ -1,5 +1,18 @@
 package frc.robot.subsystems.climb;
 
-public class climbIO {
+import org.littletonrobotics.junction.AutoLog;
 
+public interface climbIO {
+
+  @AutoLog
+  public static class climbIOInputs {
+    public double velocityRotsPerSec = 0.0;
+    public double appliedVolts = 0.0;
+    public double statorCurrentAmps = 0.0;
+    public double supplyCurrentAmps = 0.0;
+  }
+
+  public default void updateInputs(climbIOInputs inputs) {}
+
+  public default void runVolts(double voltage) {}
 }
