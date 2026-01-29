d9fbb442698fa5141df3a758339b13f7809dc1b8
TheDeadChicken <mishi.doron@gmail.com>
2026-01-28 18:12:27 -0800

    Setup Climb

--- src/main/java/frc/robot/subsystems/climb/climb.java
+++ src/main/java/frc/robot/subsystems/climb/climb.java
@@ -1,3 +1,33 @@
 package frc.robot.subsystems.climb;
 
-public class Climb {}
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.rollers.spindexer.SpindexerIO;
+import frc.robot.subsystems.rollers.spindexer.SpindexerIOInputsAutoLogged;
+
+import static frc.robot.subsystems.climb.ClimbConstants.*;
+
+public class Climb extends SubsystemBase {
+
+    private ClimbIO io;
+    private final ClimbIOInputsAutoLogged inputs = new ClimbIOInputsAutoLogged();
+  
+  public Climb(ClimbIO io) {
+    this.io = io;
+  }
+
+
+    public void periodic() {
+        io.updateInputs(inputs);
+    }  
+
+
+    public Command climbUp() {
+        return run(() -> io.setPosition(upRotations));
+    }
+
+    public Command climbDown() {
+        return run(() -> io.setPosition(downRotations));
+    }
+}
+
