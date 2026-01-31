19016234191531b47f0ff7501cf57855a4715aef
Benson Liu <benliu7722@gmail.com>
2026-01-31 11:49:20 -0800

    moved constants over to shooterConstants

--- src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
@@ -1,5 +1,9 @@
 package frc.robot.subsystems.shooter;
 
 public class ShooterConstants {
+  public static final double Gravity = 9.81;
+  public static final double ShooterMaxRPM = 3500;
+  public static final double ShooterMinAngle = 45;
+  public static final double ShooterMaxAngle = 75;
     
 }
