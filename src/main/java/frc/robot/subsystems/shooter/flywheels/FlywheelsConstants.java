036b2f0d0469159fbdb0ba1b7cac58c82fbb977b
TheDeadChicken <mishi.doron@gmail.com>
2026-02-03 18:19:48 -0800

    Finalize flywheel constants

--- src/main/java/frc/robot/subsystems/shooter/flywheels/FlywheelsConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/flywheels/FlywheelsConstants.java
@@ -3,26 +3,26 @@ package frc.robot.subsystems.shooter.flywheels;
 import frc.robot.util.LoggedTunableNumber;
 
 public class FlywheelsConstants {
-  public static int leaderID = 0;
-  public static int followID = 0;
+  public static final int leaderID = 0;
+  public static final int followID = 0;
 
-  public static LoggedTunableNumber motionMagicCruiseVelocity =
+  public static final LoggedTunableNumber motionMagicCruiseVelocity =
       new LoggedTunableNumber("Flywheels/MotionMagic/CruiseVelocity", 80); // rps
-  public static LoggedTunableNumber motionMagicAcceleration =
+  public static final LoggedTunableNumber motionMagicAcceleration =
       new LoggedTunableNumber("Flywheels/MotionMagic/Acceleration", 180);
-  public static LoggedTunableNumber motionMagicJerk =
+  public static final LoggedTunableNumber motionMagicJerk =
       new LoggedTunableNumber("Flywheels/MotionMagic/Jerk", 1600);
 
-  public static LoggedTunableNumber readyVolts = new LoggedTunableNumber("Flywheels/readyVolts", 6);
+  public static final LoggedTunableNumber readyVolts = new LoggedTunableNumber("Flywheels/readyVolts", 6);
 
-  public static LoggedTunableNumber shooterVelocity =
+  public static final LoggedTunableNumber shooterVelocity =
       new LoggedTunableNumber("Flywheels/shootVelocity", 2000);
 
-  public static LoggedTunableNumber kV = new LoggedTunableNumber("Flywheels/kV", 0);
-  public static LoggedTunableNumber kA = new LoggedTunableNumber("Flywheels/kA", 0);
-  public static LoggedTunableNumber kP = new LoggedTunableNumber("Flywheels/kP", 0);
-  public static LoggedTunableNumber kD = new LoggedTunableNumber("Flywheels/kD", 0);
+  public static final LoggedTunableNumber kV = new LoggedTunableNumber("Flywheels/kV", 0);
+  public static final LoggedTunableNumber kA = new LoggedTunableNumber("Flywheels/kA", 0);
+  public static final LoggedTunableNumber kP = new LoggedTunableNumber("Flywheels/kP", 0);
+  public static final LoggedTunableNumber kD = new LoggedTunableNumber("Flywheels/kD", 0);
 
-  public static LoggedTunableNumber flywheelTolerance =
+  public static final LoggedTunableNumber flywheelTolerance =
       new LoggedTunableNumber("Flywheels/tolerance", 5); // TODO tune
 }
