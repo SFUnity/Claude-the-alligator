046c7725dd31189fc238a04e31035f86b3521246
Ishaan Shrimali <ishaan1003@gmail.com>
2026-02-07 13:55:01 -0800

    formating

--- src/main/java/frc/robot/subsystems/rollers/kicker/KickerConstants.java
+++ src/main/java/frc/robot/subsystems/rollers/kicker/KickerConstants.java
@@ -8,10 +8,10 @@ public class KickerConstants {
   public static final int positionFactor = 3;
 
   public static final double spinupTime = 0.25;
-  public static final LoggedTunableNumber kV = new LoggedTunableNumber("Kicker/kV",0);
-  public static final LoggedTunableNumber kA = new LoggedTunableNumber("Kicker/kA",0);
-  public static final LoggedTunableNumber kP = new LoggedTunableNumber("Kicker/kP",0);
-  public static final LoggedTunableNumber kD = new LoggedTunableNumber("Kicker/kD",0);
+  public static final LoggedTunableNumber kV = new LoggedTunableNumber("Kicker/kV", 0);
+  public static final LoggedTunableNumber kA = new LoggedTunableNumber("Kicker/kA", 0);
+  public static final LoggedTunableNumber kP = new LoggedTunableNumber("Kicker/kP", 0);
+  public static final LoggedTunableNumber kD = new LoggedTunableNumber("Kicker/kD", 0);
 
   public static final LoggedTunableNumber kickerSpeedVolts =
       new LoggedTunableNumber("Kicker/rollerSpeed", 2);
