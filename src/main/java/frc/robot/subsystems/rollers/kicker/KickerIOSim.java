da81ded6800fa7a8ec5a814ab61bab5064708c3f
Lucas Lee <llee28@urbanschool.org>
2026-02-08 13:59:39 -0800

    format

--- src/main/java/frc/robot/subsystems/rollers/kicker/KickerIOSim.java
+++ src/main/java/frc/robot/subsystems/rollers/kicker/KickerIOSim.java
@@ -1,15 +1,14 @@
 package frc.robot.subsystems.rollers.kicker;
 
-import frc.robot.util.LoggedTunableNumber;
 import static frc.robot.subsystems.rollers.kicker.KickerConstants.*;
 
 import edu.wpi.first.math.controller.PIDController;
+import frc.robot.util.LoggedTunableNumber;
 
 public class KickerIOSim implements KickerIO {
   private double appliedVolts = 0.0;
   private double currentAmps = 0.0;
   private final PIDController controller;
-  
 
   public KickerIOSim() {
     controller = new PIDController(kP.get(), 0.0, 0.0);
