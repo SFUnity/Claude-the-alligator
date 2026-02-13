b240ec243bda84fd2ee17be112898c1b2b782f34
MrTinker64 <avi.micah@gmail.com>
2026-02-12 17:59:58 -0800

    remove old + rename

--- src/main/java/frc/robot/subsystems/shooter/ShooterVisualizer.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterVisualizer.java
@@ -18,24 +18,21 @@ public class ShooterVisualizer {
   private final LoggedMechanismRoot2d turretRoot;
   private final LoggedMechanismLigament2d turret;
   private final LoggedMechanismLigament2d hood;
-  private LoggedMechanismLigament2d shooterVisualizer;
-  private double xCoord = 0; // TODO update
-  private double yCoord = 0;
-  private double zCoord = 0;
   private final String key;
 
-  private final double turretDiameter = Units.inchesToMeters(8);
+  private final double turretRadius = Units.inchesToMeters(4);
+  private final double hoodHeight = Units.inchesToMeters(8);
 
   public ShooterVisualizer(String key, Color color) {
     this.key = key;
 
     hoodMechanism = new LoggedMechanism2d(0, 0, new Color8Bit(Color.kBlack));
-    hoodRoot = hoodMechanism.getRoot("Shooter Root", xCoord, yCoord);
-    hood = new LoggedMechanismLigament2d("Hood", 0, 0);
+    hoodRoot = hoodMechanism.getRoot("Shooter Root", 0, 0);
+    hood = new LoggedMechanismLigament2d("Hood", hoodHeight, 0);
 
     turretMechanism = new LoggedMechanism2d(0, 0, new Color8Bit(Color.kBlack));
-    turretRoot = turretMechanism.getRoot("Turret Root", xCoord, yCoord);
-    turret = new LoggedMechanismLigament2d("Turret", turretDiameter, 0, 8,  new Color8Bit(color));
+    turretRoot = turretMechanism.getRoot("Turret Root", 0, 0);
+    turret = new LoggedMechanismLigament2d("Turret", turretRadius, 0, 8,  new Color8Bit(color));
 
     turretRoot.append(turret);
     hoodRoot.append(hood);
@@ -46,12 +43,7 @@ public class ShooterVisualizer {
     hood.setAngle(hoodAngle);
     Logger.recordOutput("Subsystems/Shooter/Hood/Mechanism2D/" + key, hoodMechanism);
 
-    Pose3d shooterPose =
-        new Pose3d(
-            xCoord + Math.cos(turretAngle) * Math.cos(hoodAngle),
-            yCoord + Math.sin(turretAngle) * Math.cos(hoodAngle),
-            zCoord + Math.sin(hoodAngle),
-            new Rotation3d());
+    Pose3d shooterPose = new Pose3d();
     Logger.recordOutput("Subsystems/Shooter/Mechanism3D/" + key, shooterPose);
   }
 }
