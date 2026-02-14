533a897678046c5aebbd02a30b410c2c388ba79c
SeanC0112 <146582100+SeanC0112@users.noreply.github.com>
2026-02-14 14:55:49 -0800

    add bang bang overrides to kicker talon fx

--- src/main/java/frc/robot/subsystems/rollers/kicker/KickerIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/rollers/kicker/KickerIOTalonFX.java
@@ -5,10 +5,14 @@ import static frc.robot.subsystems.rollers.kicker.KickerConstants.*;
 import static frc.robot.util.PhoenixUtil.tryUntilOk;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.VelocityDutyCycle;
+import com.ctre.phoenix6.controls.VelocityTorqueCurrentFOC;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj.DutyCycle;
 
 public class KickerIOTalonFX implements KickerIO {
   private final TalonFX rollerMotor = new TalonFX(kickerMotorID);
@@ -16,10 +20,12 @@ public class KickerIOTalonFX implements KickerIO {
 
   private final VoltageOut voltageOut =
       new VoltageOut(0).withEnableFOC(true).withUpdateFreqHz(loopPeriodSecs);
+  private final VelocityDutyCycle dutyCycle = new VelocityDutyCycle(10);
 
   public KickerIOTalonFX() {
     var talonFXConfigs = new TalonFXConfiguration();
     var slot0Configs = talonFXConfigs.Slot0;
+  
     // slot0Configs.kS = 0;
     // slot0Configs.kV = kV.get();
     // slot0Configs.kA = kA.get();
@@ -45,18 +51,23 @@ public class KickerIOTalonFX implements KickerIO {
     inputs.currentAmps = rollerMotor.getSupplyCurrent().getValueAsDouble();
   }
 
-  @Override
-  public void runVolts(double volts) {
-    rollerMotor.setControl(voltageOut.withOutput(volts));
-  }
 
   @Override
   public void stop() {
     rollerMotor.setControl(voltageOut.withOutput(0));
   }
 
+  // @Override
+  // public void runVelocity(double rps) {
+  //   rollerMotor.setControl(velocityVoltage.withVelocity((rps / 60)));
+  // }
   @Override
-  public void runVelocity(double rps) {
-    rollerMotor.setControl(velocityVoltage.withVelocity((rps / 60)));
+  public void runDutyCycle(){
+    rollerMotor.setControl(dutyCycle.withEnableFOC(true));
+  }
+
+  @Override
+  public void runTorqueControl(){
+    rollerMotor.setControl(new VelocityTorqueCurrentFOC(10));
   }
 }
