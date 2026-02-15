23906b5c85f231d3f1be6f897ec3fa603d3699c3
Lucas Lee <llee28@urbanschool.org>
2026-02-15 13:36:49 -0800

    add lasercan

--- src/main/java/frc/robot/subsystems/rollers/kicker/KickerIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/rollers/kicker/KickerIOTalonFX.java
@@ -4,6 +4,8 @@ import static frc.robot.Constants.loopPeriodSecs;
 import static frc.robot.subsystems.rollers.kicker.KickerConstants.*;
 import static frc.robot.util.PhoenixUtil.tryUntilOk;
 
+import org.littletonrobotics.junction.Logger;
+
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.VelocityDutyCycle;
 import com.ctre.phoenix6.controls.VelocityTorqueCurrentFOC;
@@ -11,8 +13,14 @@ import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import au.grapplerobotics.LaserCan;
+import au.grapplerobotics.ConfigurationFailedException;
+import edu.wpi.first.wpilibj.Alert;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Alert.AlertType;
 
 public class KickerIOTalonFX implements KickerIO {
+  private LaserCan lc;
   private final TalonFX rollerMotor = new TalonFX(kickerMotorID);
   private final VelocityVoltage velocityVoltage = new VelocityVoltage(0).withEnableFOC(true);
 
@@ -20,10 +28,19 @@ public class KickerIOTalonFX implements KickerIO {
       new VoltageOut(0).withEnableFOC(true).withUpdateFreqHz(loopPeriodSecs);
   private final VelocityDutyCycle dutyCycle = new VelocityDutyCycle(10);
   private final VelocityTorqueCurrentFOC torqueCurrent = new VelocityTorqueCurrentFOC(10);
-
+ @SuppressWarnings("resource")
   public KickerIOTalonFX() {
     var talonFXConfigs = new TalonFXConfiguration();
     var slot0Configs = talonFXConfigs.Slot0;
+    lc = new LaserCan(0);
+
+    try{
+      lc.setRangingMode(LaserCan.RangingMode.SHORT);
+      lc.setRegionOfInterest(new LaserCan.RegionOfInterest(8,8,16,16));
+      lc.setTimingBudget(LaserCan.TimingBudget.TIMING_BUDGET_33MS);
+    } catch (ConfigurationFailedException e) {
+      new Alert("Configuration failed"+e, AlertType.kError).set(true);
+    };
 
     // slot0Configs.kS = 0;
     // slot0Configs.kV = kV.get();
@@ -49,6 +66,13 @@ public class KickerIOTalonFX implements KickerIO {
     inputs.appliedVolts = rollerMotor.getMotorVoltage().getValueAsDouble();
     inputs.currentAmps = rollerMotor.getSupplyCurrent().getValueAsDouble();
     inputs.velocityRotsPerMin = rollerMotor.getVelocity().getValueAsDouble() * 60;
+    LaserCan.Measurement measurement = lc.getMeasurement();
+    if(measurement != null && measurement.status == LaserCan.LASERCAN_STATUS_VALID_MEASUREMENT){
+      new Alert("The target is" + measurement.distance_mm + "mm away", AlertType.kError).set(true);
+    }
+    else{
+      new Alert("Target out of range", AlertType.kError).set(true);
+    }
   }
 
   @Override
