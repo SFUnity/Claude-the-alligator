04dabe4f8bef77def4eb385373ef09732c146c96
SF Unity <sfunityrobotics@gmail.com>
2026-02-21 12:08:05 -0800

    wrap to 90

--- src/main/java/frc/robot/subsystems/drive/Module.java
+++ src/main/java/frc/robot/subsystems/drive/Module.java
@@ -66,7 +66,7 @@ public class Module {
     }
 
     Logger.recordOutput(
-        "Odometry/WrappedModuleAngles/Measured" + index, Math.abs(getAngle().getDegrees() % 180));
+        "Odometry/WrappedModuleAngles/Measured" + index, Math.abs(getAngle().getDegrees() % 90));
 
     // Update alerts
     driveDisconnectedAlert.set(!inputs.driveConnected);
@@ -86,7 +86,7 @@ public class Module {
 
     // Log
     Logger.recordOutput(
-        "Odometry/WrappedModuleAngles/Setpoints" + index, Math.abs(state.angle.getDegrees() % 180));
+        "Odometry/WrappedModuleAngles/Setpoints" + index, Math.abs(state.angle.getDegrees() % 90));
   }
 
   /** Runs the module with the specified output while controlling to zero degrees. */
