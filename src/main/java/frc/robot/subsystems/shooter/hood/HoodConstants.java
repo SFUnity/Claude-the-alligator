080fbb27fd847196ba413c0275918648cf2a1286
Lucas Lee <llee28@urbanschool.org>
2026-02-22 12:11:49 -0800

    add variables to hood constants

--- src/main/java/frc/robot/subsystems/shooter/hood/HoodConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/hood/HoodConstants.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.shooter.hood;
 
+import edu.wpi.first.math.util.Units;
 import frc.robot.util.LoggedTunableNumber;
 
 public class HoodConstants {
@@ -7,10 +8,16 @@ public class HoodConstants {
   public static final double gearRatio = 73.5;
 
   public static final double angleTolerance = 2;
+   public static final double minAngleRads = Units.degreesToRadians(14.06);
+  public static final double maxAngleRads = Units.degreesToRadians(46.5);
+  public static final double armLengthMeters = Units.inchesToMeters(15);
+  public static final double intakePIDMinInput = 0;
+  public static final double intakePIDMaxInput = 1 * 360;
+
 
   // TODO add values
-  public static double minPositionDegs;
-  public static double maxPositionDegs;
+  public static final double minPositionDegs = 14.06;
+  public static final double maxPositionDegs = 46.5;
 
   public static LoggedTunableNumber kP = new LoggedTunableNumber("Hood/kP", 0);
   public static LoggedTunableNumber kD = new LoggedTunableNumber("Hood/kD", 0);
