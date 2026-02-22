9573e6cab2e9edccac9ed2624322e5a5c5f3855f
Angelina Liu <liu.angelinaca@gmail.com>
2026-02-22 14:24:30 -0800

    add DoubleUpperScoreCenter cmds

--- src/main/java/frc/robot/Autos.java
+++ src/main/java/frc/robot/Autos.java
@@ -96,6 +96,7 @@ public class Autos {
     chooser.addRoutine("Lower Feed Score", this::LowerFeedScoreAutoRoutine);
     chooser.addRoutine("Upper Feed Score", this::UpperFeedScoreAutoRoutine);
     chooser.addRoutine("Double Score Center", this::DoubleScoreCenter);
+    chooser.addRoutine("Double Upper Score Center", this::DoubleUpperScoreCenter);
 
     // not as necessary
     chooser.addRoutine("Climb", this::climbAutoRoutine);
@@ -385,4 +386,18 @@ public class Autos {
         .onTrue(RobotCommands.readyThenShoot(shooter, kicker, spindexer).withTimeout(5));
     return routine;
   }
+
+  public AutoRoutine DoubleUpperScoreCenter() {
+    AutoRoutine routine = factory.newRoutine("Double Upper Score Center Auto Routine");
+    AutoTrajectory DoubleUpperScoreCenter = routine.trajectory("DoubleUpperScoreCenter");
+    routine
+        .active()
+        .onTrue(Commands.sequence(DoubleUpperScoreCenter.resetOdometry(), DoubleUpperScoreCenter.cmd()));
+    DoubleUpperScoreCenter.atTime("StartIntake").onTrue(RobotCommands.intake(intake, intakePivot));
+    DoubleUpperScoreCenter.atTime("StopIntake").onTrue(RobotCommands.jork(intake, intakePivot));
+    DoubleUpperScoreCenter.atTime("StartShoot")
+        .onTrue(RobotCommands.readyThenShoot(shooter, kicker, spindexer).withTimeout(5));
+    return routine;
+  }
+
 }
