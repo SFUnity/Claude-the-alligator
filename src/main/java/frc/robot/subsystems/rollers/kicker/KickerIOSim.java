f04a542aa04fec6f05b9706e0a09267064af65df
MrTinker64 <avi.micah@gmail.com>
2026-02-22 20:48:34 -0800

    remove unused

--- src/main/java/frc/robot/subsystems/rollers/kicker/KickerIOSim.java
+++ src/main/java/frc/robot/subsystems/rollers/kicker/KickerIOSim.java
@@ -1,13 +1,10 @@
 package frc.robot.subsystems.rollers.kicker;
 
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 import frc.robot.Constants;
-import frc.robot.util.LoggedTunableNumber;
 
 public class KickerIOSim implements KickerIO {
   private double appliedVolts = 0.0;
