b2d080d5be76e92a8b91d821ad15bf504dca8814
SF Unity <sfunityrobotics@gmail.com>
2026-02-25 16:57:07 -0800

    update flywheel constants

--- src/main/java/frc/robot/subsystems/shooter/flywheels/FlywheelsConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/flywheels/FlywheelsConstants.java
@@ -4,10 +4,10 @@ import edu.wpi.first.math.util.Units;
 import frc.robot.util.LoggedTunableNumber;
 
 public class FlywheelsConstants {
-  public static final int leaderID = 5;
-  public static final int followID = 6;
+  public static final int leaderID = 10;
+  public static final int followID = 11;
 
-  public static final double gearRatio = 2 / 3; // step up so less than one
+  public static final double gearRatio = 3 / 5; // step up so less than one
 
   public static final LoggedTunableNumber readyRPMSetpoint =
       new LoggedTunableNumber("Flywheels/readyRPM", 100);
