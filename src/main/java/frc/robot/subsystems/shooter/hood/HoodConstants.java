699780fc6678f892b2b8520920b41ef8b4ce2b65
SF Unity <sfunityrobotics@gmail.com>
2026-02-25 16:57:22 -0800

    update hood constants

--- src/main/java/frc/robot/subsystems/shooter/hood/HoodConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/hood/HoodConstants.java
@@ -4,14 +4,14 @@ import edu.wpi.first.math.util.Units;
 import frc.robot.util.LoggedTunableNumber;
 
 public class HoodConstants {
-  public static final int hoodMotorID = 7;
-  public static final double gearRatio = 23 * 26 / 12; // ~25
+  public static final int hoodMotorID = 12;
+  public static final double gearRatio = 12f/598f;
 
   // TODO add values
   public static final double minPositionDegs = 14.06;
-  public static final double maxPositionDegs = 46.5;
+  public static final double maxPositionDegs = 46.333;
 
-  public static final double angleTolerance = 2;
+  public static final double angleTolerance = 0.5;
   public static final double minAngleRads = Units.degreesToRadians(minPositionDegs);
   public static final double maxAngleRads = Units.degreesToRadians(maxPositionDegs);
   public static final double armLengthMeters = Units.inchesToMeters(15);
