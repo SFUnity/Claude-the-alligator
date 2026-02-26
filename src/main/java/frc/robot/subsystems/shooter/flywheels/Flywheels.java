05bfe4bb979ae76e5b454d3052b7c4a50ce2ddfb
SF Unity <sfunityrobotics@gmail.com>
2026-02-25 18:16:34 -0800

    format

--- src/main/java/frc/robot/subsystems/shooter/flywheels/Flywheels.java
+++ src/main/java/frc/robot/subsystems/shooter/flywheels/Flywheels.java
@@ -107,7 +107,7 @@ public class Flywheels extends SubsystemBase {
   }
 
   public void setIsShooting(boolean isShooting) {
-    if(state != FlywheelsState.VOLTS && state != FlywheelsState.STOP) {
+    if (state != FlywheelsState.VOLTS && state != FlywheelsState.STOP) {
       setState(isShooting ? FlywheelsState.RUN : FlywheelsState.IDLE);
     }
   }
