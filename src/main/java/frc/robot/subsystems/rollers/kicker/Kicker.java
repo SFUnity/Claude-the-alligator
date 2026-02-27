26207cf0df877c94d43ecd82c9a1cda36a7cc73a
MrTinker64 <avi.micah@gmail.com>
2026-02-27 11:33:19 -0800

    change setState to runOnce

--- src/main/java/frc/robot/subsystems/rollers/kicker/Kicker.java
+++ src/main/java/frc/robot/subsystems/rollers/kicker/Kicker.java
@@ -99,7 +99,7 @@ public class Kicker extends SubsystemBase {
   }
 
   public Command setState(KickerState state) {
-    return run(() -> this.state = state).withName("Set " + state);
+    return runOnce(() -> this.state = state).withName("Set " + state);
   }
 
   public boolean atGoal() {
