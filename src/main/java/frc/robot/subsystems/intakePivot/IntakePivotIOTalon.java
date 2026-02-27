cad310fb6110048f4ecc9397d3e1f3a0364e16da
SF Unity <sfunityrobotics@gmail.com>
2026-02-27 15:52:56 -0800

    add runVolts

--- src/main/java/frc/robot/subsystems/intakePivot/IntakePivotIOTalon.java
+++ src/main/java/frc/robot/subsystems/intakePivot/IntakePivotIOTalon.java
@@ -5,6 +5,7 @@ import static frc.robot.util.PhoenixUtil.*;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.GravityTypeValue;
 import com.ctre.phoenix6.signals.InvertedValue;
@@ -14,6 +15,7 @@ import edu.wpi.first.math.util.Units;
 public class IntakePivotIOTalon implements IntakePivotIO {
   private final TalonFX pivot = new TalonFX(pivotID);
   private PositionVoltage positionVoltage = new PositionVoltage(0.0).withEnableFOC(true);
+  private VoltageOut voltageOut = new VoltageOut(0);
 
   public IntakePivotIOTalon() {
     TalonFXConfiguration config = new TalonFXConfiguration();
@@ -48,6 +50,11 @@ public class IntakePivotIOTalon implements IntakePivotIO {
   @Override
   public void setPivotPosition(double setpointDeg) {
     pivot.setControl(positionVoltage.withPosition(Units.degreesToRotations(setpointDeg)));
+  }
+
+  @Override
+  public void runVolts(double volts) {
+    pivot.setControl(voltageOut.withOutput(volts));
   }
 
   @Override
