a597daf80d9406b5462afbf9b0ff19eb7fe53086
MrTinker64 <avi.micah@gmail.com>
2026-03-08 17:02:23 -0700

    update limelight constants

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -41,9 +41,9 @@ public class VisionConstants {
   public static final LimelightConfigs starboardFore = new LimelightConfigs("sfore", 0, 0, 0, 0, 0, 0);
   public static final LimelightConfigs starboardAft =
       new LimelightConfigs("saft", -0.308, 0.256, 0.202111, 0, 65.55, 207.785);
-  public static final LimelightConfigs portFore = new LimelightConfigs("pfore", -0.247, -0.286, 0.228, 0, 0, 45);
+  public static final LimelightConfigs portFore = new LimelightConfigs("pfore", -0.247, -0.286, 0.228, 0, 65.5, 45);
   public static final LimelightConfigs portAft =
-      new LimelightConfigs("paft", -0.308, 0.256, 0.202111, 0, 65.55, 207.785);
+      new LimelightConfigs("paft", -0.301, -0.162, 0.23, 0, 61.9, 145);
 
   // what r these
   public static final double megatag1AmbiguityMinimum = 0.7; // TODO change
