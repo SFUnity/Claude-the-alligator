50cc6a437a6f32926bbd8f2a92dba3537f59c656
SF Unity <sfunityrobotics@gmail.com>
2026-03-11 19:55:11 -0700

    foramt

--- src/main/java/frc/robot/subsystems/shooter/turret/Turret.java
+++ src/main/java/frc/robot/subsystems/shooter/turret/Turret.java
@@ -28,7 +28,8 @@ public class Turret extends SubsystemBase {
   private double positionDegs = 0;
 
   private boolean eDisabled = false; // TODO tune debouncers + their offsets
-  private Debouncer outOfBoundsDebouncer = new Debouncer(0.1, DebounceType.kRising); //TODO add stuff for if any encoder breaks
+  private Debouncer outOfBoundsDebouncer =
+      new Debouncer(0.1, DebounceType.kRising); // TODO add stuff for if any encoder breaks
   private Debouncer encoderBrokenDebouncer = new Debouncer(0.5, DebounceType.kRising);
   private double lastTalonRotations = 0;
 
@@ -204,7 +205,7 @@ public class Turret extends SubsystemBase {
   }
 
   public double getPositionDegs() {
-    return Units.rotationsToDegrees(inputs.talonRotations + talonOffsetRots);
+    return Units.rotationsToDegrees(inputs.talonRotations);
   }
 
   public boolean getEDisabled() {
