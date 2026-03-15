dd31802fd7e3d7053878686a1c140641593f5f99
LILLIAN ORMES <lillian.ormes_28@sfuhs.org>
2026-03-15 12:51:01 -0700

    change shooting time for DoubleScoreCenterLoop

--- src/main/java/frc/robot/Autos.java
+++ src/main/java/frc/robot/Autos.java
@@ -447,7 +447,7 @@ public class Autos {
         .done()
         .onTrue(
             Commands.sequence(
-                RobotCommands.readyThenShoot(shooter, kicker, spindexer).withTimeout(3),
+                RobotCommands.readyThenShoot(shooter, kicker, spindexer).withTimeout(5),
                 RobotCommands.stopShoot(shooter, kicker, spindexer).withTimeout(0.5),
                 segment1.cmd()));
     segment1.atTime("StartIntake2").onTrue(RobotCommands.intake(intake, intakePivot));
