94bac5369366dabd6d95b08ca346784294bafb15
MrTinker64 <avi.micah@gmail.com>
2026-03-16 19:07:48 -0700

    make climber useable

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -411,9 +411,6 @@ public class RobotContainer {
     controller.a().onTrue(shooter.overrideSetScoring(true));
     controller.b().onTrue(shooter.overrideSetScoring(false));
 
-    controller.povUp().whileTrue(shooter.setIsClose(true));
-    controller.povDown().whileTrue(shooter.setIsClose(false));
-
     // controller.rightTrigger().onTrue(shooter.toggleHoodIsSafe());
     // controller.rightBumper().onTrue(Commands.runOnce(() -> isShooting = !isShooting));
     // controller
