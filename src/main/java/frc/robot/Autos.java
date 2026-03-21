486be3fa05ad3a3081d1cd037a6a1d7062185058
LILLIAN ORMES <lillian.ormes_28@sfuhs.org>
2026-03-21 10:43:26 -0700

    formatting

--- src/main/java/frc/robot/Autos.java
+++ src/main/java/frc/robot/Autos.java
@@ -362,7 +362,9 @@ public class Autos {
         .done()
         .onTrue(
             Commands.sequence(
-                RobotCommands.readyThenShoot(shooter, kicker, spindexer).alongWith(RobotCommands.jork(intake, intakePivot).asProxy()).withTimeout(4.0),
+                RobotCommands.readyThenShoot(shooter, kicker, spindexer)
+                    .alongWith(RobotCommands.jork(intake, intakePivot).asProxy())
+                    .withTimeout(4.0),
                 RobotCommands.stopShoot(shooter, kicker, spindexer).withTimeout(0.5),
                 segment1.cmd()));
     segment1.atTime("StartIntake2").onTrue(RobotCommands.intake(intake, intakePivot));
@@ -371,7 +373,9 @@ public class Autos {
         .done()
         .onTrue(
             Commands.sequence(
-                RobotCommands.readyThenShoot(shooter, kicker, spindexer).alongWith(RobotCommands.jork(intake, intakePivot)).withTimeout(2.5),
+                RobotCommands.readyThenShoot(shooter, kicker, spindexer)
+                    .alongWith(RobotCommands.jork(intake, intakePivot))
+                    .withTimeout(2.5),
                 RobotCommands.stopShoot(shooter, kicker, spindexer).withTimeout(0.5)));
     return routine;
   }
