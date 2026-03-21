f3c6ff8bf6d014a30ad4595c5b9ce4aa9f602a06
MrTinker64 <avi.micah@gmail.com>
2026-03-20 23:33:11 -0700

    try switching to hood is safe to being part of spin up sequence

--- src/main/java/frc/robot/commands/RobotCommands.java
+++ src/main/java/frc/robot/commands/RobotCommands.java
@@ -19,7 +19,7 @@ public class RobotCommands {
   public static Command stopShoot(Shooter shooter, Kicker kicker, Spindexer spindexer) {
     return Commands.parallel(
             spindexer.stop(),
-            waitSeconds(0.7).andThen(shooter.setShooting(false), kicker.setState(KickerState.STOP)))
+            waitSeconds(0.7).andThen(shooter.setShooting(false), kicker.setState(KickerState.STOP), shooter.setHoodIsSafe(false)))
         .withName("StopShoot");
   }
 
@@ -27,7 +27,7 @@ public class RobotCommands {
     return Commands.parallel(
             kicker.setState(KickerState.RUN),
             shooter.setShooting(true),
-            waitSeconds(1).andThen(spindexer.run()))
+            waitSeconds(1).andThen(spindexer.run())).beforeStarting(shooter.setHoodIsSafe(true))
         .withName("ReadyThenShoot");
   }
 
