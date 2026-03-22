918011a04007cd08557d68d39a245745964779c7
MrTinker64 <avi.micah@gmail.com>
2026-03-22 08:10:24 -0700

    make sure auto shootig overrides everything except hood is safe

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -216,30 +216,30 @@ public class Shooter extends VirtualSubsystem {
         hood.setAngle(solution.hoodAngle());
         flywheels.setVelocity(solution.flywheelSpeed());
       }
-    }
 
-    // flywheels.setVelocity(fakeFlywheelVelocity.get());
-    // hood.setAngle(fakeHoodAngle.get());
+      // flywheels.setVelocity(fakeFlywheelVelocity.get());
+      // hood.setAngle(fakeHoodAngle.get());
 
-    if (!isScoring) {
-      turret.setTarget(0 - poseManager.getRotation().getDegrees());
-      hood.setAngle(feedingHoodAngle.get());
+      if (!isScoring) {
+        turret.setTarget(0 - poseManager.getRotation().getDegrees());
+        hood.setAngle(feedingHoodAngle.get());
 
-      InterpolatingDoubleTreeMap feedRealFlywheelSpeedMap = new InterpolatingDoubleTreeMap();
-      feedRealFlywheelSpeedMap.put(6.0, feedingVelocity.get());
-      feedRealFlywheelSpeedMap.put(16.0, farFeedingVelocity.get());
+        InterpolatingDoubleTreeMap feedRealFlywheelSpeedMap = new InterpolatingDoubleTreeMap();
+        feedRealFlywheelSpeedMap.put(6.0, feedingVelocity.get());
+        feedRealFlywheelSpeedMap.put(16.0, farFeedingVelocity.get());
 
-      flywheels.setVelocity(
-          feedRealFlywheelSpeedMap.get(AllianceFlipUtil.applyX(poseManager.getPose().getX())));
+        flywheels.setVelocity(
+            feedRealFlywheelSpeedMap.get(AllianceFlipUtil.applyX(poseManager.getPose().getX())));
 
-      // if (AllianceFlipUtil.applyX(poseManager.getPose().getX())
-      //     < FieldConstants.LinesVertical.oppAllianceZone) {
-      //   flywheels.setVelocity(feedingVelocity.get());
-      //   hood.setAngle(feedingHoodAngle.get());
-      // } else {
-      //   flywheels.setVelocity(farFeedingVelocity.get());
-      //   hood.setAngle(farFeedingHoodAngle.get());
-      // }
+        // if (AllianceFlipUtil.applyX(poseManager.getPose().getX())
+        //     < FieldConstants.LinesVertical.oppAllianceZone) {
+        //   flywheels.setVelocity(feedingVelocity.get());
+        //   hood.setAngle(feedingHoodAngle.get());
+        // } else {
+        //   flywheels.setVelocity(farFeedingVelocity.get());
+        //   hood.setAngle(farFeedingHoodAngle.get());
+        // }
+      }
     }
     if (!isShooting || !hoodIsSafe) {
       hood.setAngle(HoodConstants.minPositionDegs);
