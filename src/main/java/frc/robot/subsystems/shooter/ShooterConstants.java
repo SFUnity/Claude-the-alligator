a3da9225bde80dbf9fe11e5a4255351e65603a77
SeanC0112 <146582100+SeanC0112@users.noreply.github.com>
2026-03-21 22:07:55 -0700

    add turretcenter to constants

--- src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
@@ -1,7 +1,5 @@
 package frc.robot.subsystems.shooter;
 
-import edu.wpi.first.math.geometry.Rotation3d;
-import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import frc.robot.util.LoggedTunableNumber;
 
@@ -13,8 +11,6 @@ public class ShooterConstants {
   public static final double WheelRadius = 0.0508;
   public static final double TopBottomFlywheelRatio = 1.3;
 
-  public static final Transform3d turretCenter =
-      new Transform3d(-0.121, 0.133, 0.331, Rotation3d.kZero);
   public static final Translation3d hoodOffset = new Translation3d(0.038, 0, 0.197);
 
   // Trench passign stuff
