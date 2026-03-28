14e5c78f35e49d8ca439557cadc018de9907ad9c
SF Unity <sfunityrobotics@gmail.com>
2026-03-28 14:07:45 -0700

    enable LL 4

--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -100,9 +100,9 @@ public class Vision extends VirtualSubsystem {
               || estimatedPose.getX() > FieldConstants.fieldLength + fieldBorderMargin
               || estimatedPose.getY() < -fieldBorderMargin
               || estimatedPose.getY() > FieldConstants.fieldWidth + fieldBorderMargin
-              || io[i]
-                  .getName()
-                  .equals(portAft.name) // TODO make sure we remove later when camera works
+          // || io[i]
+          //     .getName()
+          //     .equals(portAft.name)
           // if too far away from current pose, depends on amount of apriltags
           // || poseManager.getDistanceTo(estimatedPose) > allowableDistance
           ;
